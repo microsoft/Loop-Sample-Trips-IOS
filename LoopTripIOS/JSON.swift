dca054affb97975f1075b475b5db8142f8a439ab
scotts@microsoft.com <scotts@microsoft.com>
2016-09-22 13:41:43 -0700

    - Changed license header to MIT  - Updated to link against LoopSDK v2.1.0  - Change version to v1.0.12

--- LoopTripIOS/JSON.swift
+++ LoopTripIOS/JSON.swift
@@ -3,21 +3,25 @@
 //  JSON utilities
 //  Trips App
 //
-//  Copyright (c) Microsoft Corporation
+//  Copyright (c) 2016 Microsoft Corporation
 //
-//  All rights reserved.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-//  Licensed under the Apache License, Version 2.0 (the License); you may not 
-//  use this file except in compliance with the License. You may obtain a copy 
-//  of the License at http://www.apache.org/licenses/LICENSE-2.0
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
 //
-//  THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS 
-//  OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY 
-//  IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, 
-//  MERCHANTABLITY OR NON-INFRINGEMENT.
-//
-//  See the Apache Version 2.0 License for specific language governing permissions 
-//  and limitations under the License.
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 import Foundation
@@ -75,7 +79,7 @@ class JSONUtils {
                         region: startLocale["region"],
                         country: startLocale["country"])
                 } else {
-                    return nil;
+                    return nil
                 }
             }(),
             endLocale: {
@@ -91,11 +95,11 @@ class JSONUtils {
                         region: endLocale["region"],
                         country: endLocale["country"])
                 } else {
-                    return nil;
+                    return nil
                 }
             }(),
             startLocationEntityId: jsonTrip["startLocationEntityId"] as? String,
             endLocationEntityId: jsonTrip["endLocationEntityId"] as? String
-        );
+        )
     }
 }
